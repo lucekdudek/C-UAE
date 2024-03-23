49049b054c86a23819c609ca718b93f0f183482e
Lucjan Dudek <lucekdudek@gmail.com>
2024-03-23 18:55:38 +0100

    Descriptions and metadata

--- Code/Compatibility.lua
+++ Code/Compatibility.lua
@@ -1,8 +1,3 @@
--- exclusionTable = {
--- 	affiliation = {"armamentId", "armamentId", "armamentId"}
--- 	affiliation = {"armamentId", "armamentId", "armamentId"}
--- }
--- CUAEAddExclusionTable(exclusionTable)
 function CUAEAddExclusionTable(exclusionTable)
 	for affiliation, armamentIds in pairs(exclusionTable) do
 		for _, armamentId in ipairs(armamentIds) do
@@ -12,32 +7,15 @@ function CUAEAddExclusionTable(exclusionTable)
 	end
 end
 
--- immunityTable = { "armamentId", "armamentId", "armamentId" }
--- CUAEAddImmunityTable(immunityTable)
 function CUAEAddImmunityTable(immunityTable)
 	for _, armamentId in ipairs(immunityTable) do
 		Cuae_ImmunityTable[armamentId] = true
 	end
 end
 
--- local settings = {
--- 	ReplaceWeapons = true,
--- 	AddWeaponComponents = true,
--- 	DisallowSilencers = true,
--- 	ReplaceArmor = true,
--- 	ExtraHandgun = true,
--- 	ExtraGrenadesCount = 2,
--- 	ExtraGrenadesChance = 50,
--- 	AllowAlternativeWeaponType = true,
--- 	ArmamentStrengthFactor = 0,
--- 	ApplyChangesInSateliteView = true,
--- }
--- CUAEForceSettings(settings)
 function CUAEForceSettings(settings)
 	for setting, value in pairs(settings) do
 		Cuae_ForcedOptions[setting] = value
 	end
 	CUAE_ApplyModOptions()
 end
-
-
